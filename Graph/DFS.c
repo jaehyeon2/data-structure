ce500a0a35ecced255b08df904181db3b77b24c6
jaehyeon2 <wol5971@gmail.com>
2021-11-08 18:54:21 +0900

    graph initial commit

---
+++ Graph/DFS.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX_VERTICES 10
+#define TRUE 1
+#define FALSE 0
+
+int visited[MAX_VERTICES];
+
+typedef struct Graph{
+    int adjMatrix[MAX_VERTICES][MAX_VERTICES];
+    int n; //the number if vertex
+}Graph;
+
+void init(Graph *g){
+    int i, j;
+    g->n=0;
+    for(i=0; i<MAX_VERTICES; i++){
+        for(j=0; j<MAX_VERTICES; j++){
+            g->adjMatrix[i][j]=0;
+        }
+    }
+}
+
+void insertVertex(Graph *g, int v){
+    if (g->n==MAX_VERTICES){
+        printf("vertex is too many\n");
+        return;
+    }
+    g->n++;
+}
+
+void insertEdge(Graph *g, int u, int v){
+    if(u>=g->n||v>=g->n){
+        printf("vertex number error. vertex number is 0~N-1\n");
+        return;
+    }
+    g->adjMatrix[u][v]=1;
+    g->adjMatrix[v][u]=1;
+}
+
+void dfs(Graph *g, int v){
+    int w;
+    visited[v]=TRUE;
+    printf("%d -> ", v);
+
+    for(w=0; w<MAX_VERTICES; w++){
+        if (g->adjMatrix[v][w]==1&&visited[w]==FALSE){
+            dfs(g, w);
+        }
+    }
+}
+
+int main(){
+    int i=0;
+    Graph g;
+
+    init(&g);
+    for(i=0; i<MAX_VERTICES; i++){
+        visited[i]=FALSE;
+    }
+    for(i=0; i<7; i++){
+        insertVertex(&g, i);
+    }
+    insertEdge(&g, 0, 4);
+    insertEdge(&g, 0, 6);
+    insertEdge(&g, 1, 3);
+    insertEdge(&g, 3, 5);
+    insertEdge(&g, 4, 1);
+    insertEdge(&g, 4, 2);
+    insertEdge(&g, 5, 2);
+
+    dfs(&g, 0);
+}
